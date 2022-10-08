97a1f4f658890f14f9b7d1700cf1ac6015b6a0e2
Konstantin Geyst <konstantin.geyst@ispringsolutions.com>
2022-10-08 14:36:29 +0300

    added test_can_remove_integers_from_hash_map

--- tests/hashmaps.c
+++ tests/hashmaps.c
@@ -16,6 +16,8 @@
 #include "../src/allocator.h"
 #include "../src/primitives.h"
 
+#define ITERATION_COUNT 1000
+
 static void create_int_hash_map_and_allocator(hmHashMap* hash_map, hmAllocator* allocator)
 {
     hmError err = hmCreateSystemAllocator(allocator);
@@ -55,7 +57,7 @@ static void test_can_put_and_get_integers_from_hash_map()
     hmAllocator allocator;
     hmHashMap hash_map;
     create_int_hash_map_and_allocator(&hash_map, &allocator);
-    for (hm_nint i = 0; i < 1000; i++) { // also tests rehashing
+    for (hm_nint i = 0; i < ITERATION_COUNT; i++) { // also tests rehashing
         hm_nint value = i*2;
         hmError err = hmHashMapPut(&hash_map, &i, &value);
         HM_TEST_ASSERT_OK(err);
@@ -67,8 +69,39 @@ static void test_can_put_and_get_integers_from_hash_map()
     dispose_memory_reader_and_allocator(&hash_map, &allocator);
 }
 
+static void test_can_remove_integers_from_hash_map()
+{
+    hmAllocator allocator;
+    hmHashMap hash_map;
+    create_int_hash_map_and_allocator(&hash_map, &allocator);
+    for (hm_nint i = 0; i < ITERATION_COUNT; i++) {
+        hm_nint value = i*2;
+        hmError err = hmHashMapPut(&hash_map, &i, &value);
+        HM_TEST_ASSERT_OK(err);
+    }
+    for (hm_nint i = 0; i < ITERATION_COUNT; i++) { // removes all non-odd elements
+        if (i % 2 == 0) {
+            hm_bool removed;
+            hmError err = hmHashMapRemove(&hash_map, &i, &removed);
+            HM_TEST_ASSERT_OK(err);
+            HM_TEST_ASSERT(removed);
+        }
+    }
+    for (hm_nint i = 0; i < ITERATION_COUNT; i++) {
+        hm_nint retrieved_value;
+        hmError err = hmHashMapGet(&hash_map, &i, &retrieved_value);
+        if (i % 2 == 0) {
+            HM_TEST_ASSERT(err == HM_ERROR_NOT_FOUND);
+        } else {
+            HM_TEST_ASSERT_OK(err);
+        }
+    }
+    dispose_memory_reader_and_allocator(&hash_map, &allocator);
+}
+
 void test_hashmaps()
 {
     test_can_create_and_dispose_hash_map();
     test_can_put_and_get_integers_from_hash_map();
+    test_can_remove_integers_from_hash_map();
 }
