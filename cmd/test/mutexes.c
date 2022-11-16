97fa8f43fe220b3164c31d426c5499e5cc9227da
Konstantin Geyst <konstantin.geyst@ispringsolutions.com>
2022-11-17 01:13:51 +0300

    added proper mutex test

--- cmd/test/mutexes.c
+++ cmd/test/mutexes.c
@@ -13,7 +13,10 @@
 
 #include "common.h"
 #include <core/allocator.h>
+#include <core/primitives.h>
+#include <collections/hashmap.h>
 #include <threading/mutex.h>
+#include <threading/thread.h>
 
 static void create_mutex_and_allocator(hmMutex* mutex, hmAllocator* allocator)
 {
@@ -50,7 +53,85 @@ static void test_can_create_lock_unlock_dispose_mutex_in_general()
     dispose_mutex_and_allocator(&mutex, &allocator);
 }
 
+typedef struct {
+    hmAllocator allocator;
+    hmMutex     mutex;
+    hmHashMap   hash_map;
+} shared_hash_map_and_mutex_context;
+
+static hmError mutexes_protect_from_data_corruption_thread_func(void* user_data)
+{
+    shared_hash_map_and_mutex_context* context = (shared_hash_map_and_mutex_context*)user_data;
+    for (hm_nint i = 0; i < 10000; i++) {
+        hmError err = hmMutexLock(&context->mutex);
+        HM_TEST_ASSERT_OK(err);
+        hm_nint key = i * 2;
+        err = hmHashMapPut(&context->hash_map, &key, &i);
+        HM_TEST_ASSERT_OK(err);
+        err = hmMutexUnlock(&context->mutex);
+        HM_TEST_ASSERT_OK(err);
+    }
+    return HM_OK;
+}
+
+static void test_mutexes_protect_from_data_corruption()
+{
+    shared_hash_map_and_mutex_context context;
+    hmError err = hmCreateSystemAllocator(&context.allocator);
+    HM_TEST_ASSERT_OK(err);
+    err = hmCreateHashMap(
+        &context.allocator,
+        &hmNintHashFunc,
+        &hmNintEqualsFunc,
+        HM_NULL,      /* key_dispose_func */
+        HM_NULL,      /* value_dispose_func */
+        sizeof(hm_nint),
+        sizeof(hm_nint),
+        HM_DEFAULT_HASHMAP_CAPACITY,
+        HM_DEFAULT_HASHMAP_LOAD_FACTOR,
+        &context.hash_map
+    );
+    HM_TEST_ASSERT_OK(err);
+    err = hmCreateMutex(
+        &context.allocator,
+        &context.mutex
+    );
+    HM_TEST_ASSERT_OK(err);
+    #define TEST_THREAD_COUNT 5
+    hmString name;
+    err = hmCreateStringViewFromCString("", &name);
+    HM_TEST_ASSERT_OK(err);
+    hmThreadProperties thread_properties;
+    thread_properties.name = &name;
+    hmThread threads[TEST_THREAD_COUNT];
+    for (hm_nint i = 0; i < TEST_THREAD_COUNT; i++) {
+        err = hmCreateThread(
+            &context.allocator,
+            thread_properties,
+            &mutexes_protect_from_data_corruption_thread_func,
+            &context,
+            &threads[i]
+        );
+        HM_TEST_ASSERT_OK(err);
+    }
+    for (hm_nint i = 0; i < TEST_THREAD_COUNT; i++) {
+        err = hmThreadJoin(&threads[i]);
+        HM_TEST_ASSERT_OK(err);
+    }
+    for (hm_nint i = 0; i < TEST_THREAD_COUNT; i++) {
+        err = hmThreadDispose(&threads[i]);
+        HM_TEST_ASSERT_OK(err);
+    }
+    err = hmHashMapDispose(&context.hash_map);
+    HM_TEST_ASSERT_OK(err);
+    err = hmMutexDispose(&context.mutex);
+    HM_TEST_ASSERT_OK(err);
+    err = hmAllocatorDispose(&context.allocator);
+    HM_TEST_ASSERT_OK(err);
+}
+
 void test_mutexes()
 {
     test_can_create_lock_unlock_dispose_mutex_in_general();
+    test_mutexes_protect_from_data_corruption();
 }
