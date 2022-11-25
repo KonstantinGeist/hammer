437f3587e82792f75567e48345993cd4446aa3a5
Konstantin Geyst <konstantin.geyst@ispringsolutions.com>
2022-11-26 01:35:54 +0300

    fixed formatting in hash.c

--- pkg/core/hash.c
+++ pkg/core/hash.c
@@ -15,19 +15,19 @@
 
 /* Based on github.com/wangyi-fudan/wyhash which is in public domain. */
 
-static hm_uint32 _wyr32(const hm_uint8 *p)
+static hm_uint32 wyr32(const hm_uint8 *p)
 {
     hm_uint32 v;
     memcpy(&v, p, sizeof(hm_uint32));
     return v;
 }
 
-static hm_uint32 _wyr24(const hm_uint8 *p, hm_uint32 k)
+static hm_uint32 wyr24(const hm_uint8 *p, hm_uint32 k)
 {
     return (((hm_uint32)p[0]) << 16) | (((hm_uint32)p[k >> 1]) << 8) | p[k - 1];
 }
 
-static void _wymix32(hm_uint32 *A, hm_uint32 *B)
+static void wymix32(hm_uint32 *A, hm_uint32 *B)
 {
     uint64_t c = *A ^ 0x53c5ca59u;
     c *= *B ^ 0x74743c1bu;
@@ -44,19 +44,19 @@ hm_uint32 hmHash(void* bytes, hm_nint size, hm_uint32 salt)
     hm_uint64 i = size;
     hm_uint32 see1 = (hm_uint32)size;
     salt ^= (hm_uint32)(size >> 32);
-    _wymix32(&salt, &see1);
-    for (; i > 8; i-= 8, p+=8) {
-        salt^=_wyr32(p);
-        see1^=_wyr32(p + 4);
-        _wymix32(&salt, &see1);
+    wymix32(&salt, &see1);
+    for (; i > 8; i -= 8, p += 8) {
+        salt ^= wyr32(p);
+        see1 ^= wyr32(p + 4);
+        wymix32(&salt, &see1);
     }
-    if (i>=4) {
-        salt^=_wyr32(p);
-        see1^=_wyr32(p + i - 4);
+    if (i >= 4) {
+        salt ^= wyr32(p);
+        see1 ^= wyr32(p + i - 4);
      } else if (i) {
-        salt^=_wyr24(p, (hm_uint32)i);
+        salt ^= wyr24(p, (hm_uint32)i);
      }
-    _wymix32(&salt, &see1);
-    _wymix32(&salt, &see1);
+    wymix32(&salt, &see1);
+    wymix32(&salt, &see1);
     return salt ^ see1;
 }
