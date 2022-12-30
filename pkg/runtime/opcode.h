a648e2da2674dbd291bad982724ca2a071c177db
Konstantin Geyst <konstantin.geyst@ispringsolutions.com>
2022-12-30 12:22:02 +0300

    decided to use typeless opcodes after all

--- pkg/runtime/opcode.h
+++ pkg/runtime/opcode.h
@@ -19,26 +19,15 @@
 /* A method's body consists of a sequence of opcodes (bytecode). The interpreter reads opcodes and executes them.
    Many opcodes are followed by additional encodings: what argument to load, what constant to push etc. */
 typedef hm_uint8 hmOpcode;
-#define HM_OPCODE_NOP      ((hmOpcode)0)  /* nop Do nothing (No operation). */
-#define HM_OPCODE_STLOC32  ((hmOpcode)1)  /* stloc.32 <uint16(N)> Pop a 32-bit value from the stack into the variable space at offset N. */
-#define HM_OPCODE_STLOC64  ((hmOpcode)2)  /* stloc.64 <uint16(N)> Pop a 64-bit value from the stack into the variable space at offset N. */
-#define HM_OPCODE_LDARG32  ((hmOpcode)3)  /* ldarg.32 <uint16(N)> Load a 32-bit argument at offset N in the argument space onto the stack. */
-#define HM_OPCODE_LDARG64  ((hmOpcode)4)  /* ldarg.64 <uint16(N)> Load a 64-bit argument at offset N in the argument space onto the stack. */
-#define HM_OPCODE_LDLOC32  ((hmOpcode)5)  /* ldloc.32 <uint16(N)> Load a 32-bit value at offset N in the variable space onto the stack. */
-#define HM_OPCODE_LDLOC64  ((hmOpcode)6)  /* ldloc.64 <uint16(N)> Load a 64-bit value at offset N in the variable space onto the stack. */
-#define HM_OPCODE_LDARGA   ((hmOpcode)7)  /* ldarga <uint16(N)> Fetch the address of an argument at offset N in the argument space and push it to the stack. */
-#define HM_OPCODE_STARG32  ((hmOpcode)8)  /* starg.32 <uint16(N)> Store a 32-bit value from the stack to the argument at offset N in the argument space. */
-#define HM_OPCODE_STARG64  ((hmOpcode)9)  /* starg.64 <uint16(N)> Store a 64-bit value from the stack to the argument at offset N in the argument space. */
-#define HM_OPCODE_LDLOCA   ((hmOpcode)10) /* ldloca <uint16(N)> Fetch the address of a variable at offset N in the variable space and push it to the stack. */
-#define HM_OPCODE_LDC32    ((hmOpcode)11) /* ldc.32 <any32(N)> Push a 32-bit constant onto the stack. */
-#define HM_OPCODE_LDC64    ((hmOpcode)12) /* ldc.32 <any64(N)> Push a 64-bit constant onto the stack. */
-#define HM_OPCODE_DUP32    ((hmOpcode)13) /* dup.32 Duplicate a 32-bit value on the top of the stack. */
-#define HM_OPCODE_DUP64    ((hmOpcode)14) /* dup.64 Duplicate a 64-bit value on the top of the stack. */
-#define HM_OPCODE_POP32    ((hmOpcode)15) /* pop.32 Pop a 32-bit value from the stack. */
-#define HM_OPCODE_POP64    ((hmOpcode)16) /* pop.64 Pop a 64-bit value from the stack. */
-#define HM_OPCODE_CALL     ((hmOpcode)17) /* call <uint32(N)> Call a method with ID = N. */
-
-// TODO push string constant (or, rather, a blob)
-// TODO finish the opcode list
+#define HM_OPCODE_NOP    ((hmOpcode)0)  /* nop Do nothing (No operation). */
+#define HM_OPCODE_STLOC  ((hmOpcode)1)  /* stloc <uint16(N)> Pop a value from the stack into the variable space at index N. */
+#define HM_OPCODE_LDARG  ((hmOpcode)2)  /* ldarg <uint16(N)> Load an argument at index N in the argument space onto the stack. */
+#define HM_OPCODE_LDLOC  ((hmOpcode)3)  /* ldloc <uint16(N)> Load a value at index N in the variable space onto the stack. */
+#define HM_OPCODE_STARG  ((hmOpcode)4)  /* starg <uint16(N)> Store a value from the stack to the argument at index N in the argument space. */
+#define HM_OPCODE_LDC32  ((hmOpcode)5)  /* ldc.32 <any32(N)> Push a 32-bit constant onto the stack. */
+#define HM_OPCODE_LDC64  ((hmOpcode)6)  /* ldc.64 <any64(N)> Push a 64-bit constant onto the stack. */
+#define HM_OPCODE_DUP    ((hmOpcode)7)  /* dup Duplicate a value on the top of the stack. */
+#define HM_OPCODE_POP    ((hmOpcode)8)  /* pop Pop a value from the stack. */
+#define HM_OPCODE_CALL   ((hmOpcode)9)  /* call <uint32(N)> Call a method with ID = N. */
 
 #endif /* HM_OPCODE_H */
