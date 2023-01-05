d114be806dc693e6598dfddee6cbf7eb639ce912
Konstantin Geyst <konstantin.geyst@ispringsolutions.com>
2023-01-05 15:26:30 +0300

    added OOM injection in module tests

--- cmd/test/modules.c
+++ cmd/test/modules.c
@@ -17,23 +17,25 @@
 
 static void create_module_registry_and_allocator(hmModuleRegistry* module_registry, hmAllocator* allocator)
 {
-    hmError err = hmCreateSystemAllocator(allocator);
+    HM_TEST_INIT_ALLOC(allocator);
+    HM_TEST_TRACK_OOM(allocator, HM_FALSE);
+    hmError err = hmCreateModuleRegistry(allocator, module_registry);
     HM_TEST_ASSERT_OK(err);
-    err = hmCreateModuleRegistry(allocator, module_registry);
-    HM_TEST_ASSERT_OK(err);
-    hmString image_path;
-    err = hmCreateStringViewFromCString("../cmd/test/data/modules.hma", &image_path);
-    HM_TEST_ASSERT_OK(err);
-    err = hmModuleRegistryLoadFromImage(module_registry, &image_path);
-    HM_TEST_ASSERT_OK(err);
+    HM_TEST_TRACK_OOM(allocator, HM_TRUE);
 }
 
 static void dispose_module_registry_and_allocator(hmModuleRegistry* module_registry, hmAllocator* allocator)
 {
     hmError err = hmModuleRegistryDispose(module_registry);
     HM_TEST_ASSERT_OK(err);
-    err = hmAllocatorDispose(allocator);
-    HM_TEST_ASSERT_OK(err);
+    HM_TEST_DEINIT_ALLOC(allocator);
+}
+
+static hmError load_image(hmModuleRegistry* module_registry)
+{
+    hmString image_path;
+    HM_TRY(hmCreateStringViewFromCString("../cmd/test/data/modules.hma", &image_path));
+    return hmModuleRegistryLoadFromImage(module_registry, &image_path);
 }
 
 static void test_can_load_existing_module()
@@ -44,12 +46,14 @@ static void test_can_load_existing_module()
     hmAllocator allocator;
     hmModuleRegistry module_registry;
     create_module_registry_and_allocator(&module_registry, &allocator);
+    hmError err = load_image(&module_registry);
+    HM_TEST_ASSERT_OK_OR_OOM(err);
     hmString core_module_name;
-    hmError err = hmCreateStringViewFromCString(CORE_MODULE_NAME, &core_module_name);
-    HM_TEST_ASSERT_OK(err);
+    err = hmCreateStringViewFromCString(CORE_MODULE_NAME, &core_module_name);
+    HM_TEST_ASSERT_OK_OR_OOM(err);
     hmModule* module = HM_NULL;
     err = hmModuleRegistryGetModuleRefByName(&module_registry, &core_module_name, &module);
-    HM_TEST_ASSERT_OK(err);
+    HM_TEST_ASSERT_OK_OR_OOM(err);
     HM_TEST_ASSERT(module != HM_NULL);
     HM_TEST_ASSERT(hmStringEqualsToCString(&hmModuleGetName(module), CORE_MODULE_NAME));
     HM_TEST_ASSERT(hmModuleGetID(module) == 1);
@@ -57,19 +61,20 @@ static void test_can_load_existing_module()
     err = hmCreateStringViewFromCString(POINT_CLASS_NAME, &point_class_name);
     hmClass* hm_class = HM_NULL;
     err = hmModuleGetClassRefByName(module, &point_class_name, &hm_class);
-    HM_TEST_ASSERT_OK(err);
+    HM_TEST_ASSERT_OK_OR_OOM(err);
     HM_TEST_ASSERT(hm_class != HM_NULL);
     HM_TEST_ASSERT(hmStringEqualsToCString(&hmClassGetName(hm_class), POINT_CLASS_NAME));
     HM_TEST_ASSERT(hmClassGetID(hm_class) == 1);
     hmString foo_method_name;
     err = hmCreateStringViewFromCString(FOO_METHOD_NAME, &foo_method_name);
-    HM_TEST_ASSERT_OK(err);
+    HM_TEST_ASSERT_OK_OR_OOM(err);
     hmMethod* method = HM_NULL;
     err = hmClassGetMethodRefByName(hm_class, &foo_method_name, &method);
-    HM_TEST_ASSERT_OK(err);
+    HM_TEST_ASSERT_OK_OR_OOM(err);
     HM_TEST_ASSERT(method != HM_NULL);
     HM_TEST_ASSERT(hmStringEqualsToCString(&hmMethodGetName(method), FOO_METHOD_NAME));
     HM_TEST_ASSERT(hmMethodGetID(method) == 1);
+HM_TEST_ON_FINALIZE
     dispose_module_registry_and_allocator(&module_registry, &allocator);
 }
 
@@ -79,17 +84,20 @@ static void test_cannot_load_non_existing_module()
     hmAllocator allocator;
     hmModuleRegistry module_registry;
     create_module_registry_and_allocator(&module_registry, &allocator);
+    hmError err = load_image(&module_registry);
+    HM_TEST_ASSERT_OK_OR_OOM(err);
     hmString non_existing_module_name;
-    hmError err = hmCreateStringViewFromCString(NON_EXISTING_MODULE_NAME, &non_existing_module_name);
-    HM_TEST_ASSERT_OK(err);
+    err = hmCreateStringViewFromCString(NON_EXISTING_MODULE_NAME, &non_existing_module_name);
+    HM_TEST_ASSERT_OK_OR_OOM(err);
     hmModule* module = HM_NULL;
     err = hmModuleRegistryGetModuleRefByName(&module_registry, &non_existing_module_name, &module);
     HM_TEST_ASSERT(err == HM_ERROR_NOT_FOUND);
     HM_TEST_ASSERT(module == HM_NULL);
+HM_TEST_ON_FINALIZE
     dispose_module_registry_and_allocator(&module_registry, &allocator);
 }
 
 HM_TEST_SUITE_BEGIN(modules)
-    HM_TEST_RUN_WITHOUT_OOM(test_can_load_existing_module)
-    HM_TEST_RUN_WITHOUT_OOM(test_cannot_load_non_existing_module)
+    HM_TEST_RUN(test_can_load_existing_module)
+    HM_TEST_RUN(test_cannot_load_non_existing_module)
 HM_TEST_SUITE_END()
